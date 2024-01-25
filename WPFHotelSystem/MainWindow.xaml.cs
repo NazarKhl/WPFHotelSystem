26c92aa5e9b43340e2960ddef075a1683ada6174
136504 <136504@M60>
2024-01-25 15:16:55 +0100

    Add project files.

---
+++ WPFHotelSystem/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using WPFHotelSystem.Models;
+
+namespace WPFHotelSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.BaseAddress = new Uri("https://localhost:7260/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private void SowPeople_Click(object sender, RoutedEventArgs e)
+        {
+            this.GetPeople();
+        }
+        private async void GetPeople()
+        {
+            var response = await client.GetStringAsync("People");
+            var people = JsonConvert.DeserializeObject<List<People>>(response);
+            dgPeople.ItemsSource = people;
+        }
+        private async void SavePeople(People people)
+        {
+            await client.PostAsJsonAsync("People/", people);
+        }
+        private async void UpdatePeople(People people)
+        {
+            await client.PutAsJsonAsync("People/" + people.PeopleId, people);
+        }
+
+        private async void DeletePeople(int PeopleId)
+        {
+            await client.DeleteAsync("People/" + PeopleId);
+        }
+
+        private void SavePeople_Click(object sender, RoutedEventArgs e)
+        {
+            var people = new People()
+            {
+                PeopleId = Convert.ToInt32(txtPeopleId.Text),
+                PeopleName = txtPeopleName.Text,
+                PeopleRoom = Convert.ToInt32(txtPeopleRoom.Text)
+            };
+
+            if (people.PeopleId == 0)
+            {
+                this.SavePeople(people);
+            }
+            else
+            {
+                this.UpdatePeople(people);
+            }
+            txtPeopleId.Text = 0.ToString();
+            txtPeopleName.Text = "";
+            txtPeopleRoom.Text = "";
+        }
+
+        private void dgPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        void EditPeople(object sender, RoutedEventArgs e)
+        {
+            People people = ((FrameworkElement)sender).DataContext as People;
+            txtPeopleId.Text = people.PeopleId.ToString();
+            txtPeopleName.Text = people.PeopleName;
+            txtPeopleRoom.Text = people.PeopleRoom.ToString();
+        }
+
+        void DeletePeople(object sender, RoutedEventArgs e)
+        {
+            People people = ((FrameworkElement)sender).DataContext as People;
+            this.DeletePeople(people.PeopleId);
+        }
+    }
+}
---
+++ WPFHotelSystem/Models/People.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFHotelSystem.Models
+{
+    public class People
+    {
+        public int PeopleId { get; set; }
+        public string PeopleName { get; set; }
+        public int PeopleRoom { get; set; }
+    }
+}
